419310a7164d40c653f80697e64a51352b951420
Alexandros <aldor94@gmail.com>
2015-10-19 20:19:23 +0300

    Subtract always active devices from currently active devices

--- counting_users.sh
+++ counting_users.sh
@@ -8,6 +8,7 @@
 # Requirements: nmap
 #
 #----------------------------------------------------------------------------------------------------------------------------------------------------------
+alwaysActiveDevices=2
 outputDirectory="/var/www/html" # Change this directory in order to change where the script outputs the files
 if [[ ! -d $outputDirectory ]]; then
   mkdir -p $outputDirectory
@@ -16,4 +17,5 @@ networksIp=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}') # Find
 subnetwork=$(echo $networksIp| awk -F/ '{print $2}') # Get networks bit-length of the prefix.
 networksIp=$(echo $networksIp | awk -F. '{OFS="."; $4='0'; print $0}')/$subnetwork # Replace the 4th ip octave with 0.
 sudo nmap -sP $networksIp | grep ..:..:..:..:..:.. | awk '{print $3}' > $outputDirectory/mac_addresses.txt # Write mac addresses to mac_addresses.txt
-wc -l < $outputDirectory/mac_addresses.txt > $outputDirectory/hackers.txt # Output the number of active users to hackers.txt
+activeDevices=$(wc -l < $outputDirectory/mac_addresses.txt) # Count the active devices
+echo $(($activeDevices-$alwaysActiveDevices))> $outputDirectory/hackers.txt # Output the number of active users to hackers.txt
