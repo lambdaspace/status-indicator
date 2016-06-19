b099bd628b87195b9fa0198b08780798797202ad
Alexandros <aldor94@gmail.com>
2016-06-19 16:01:45 +0300

    Publish num of devices only if there is an update
    Read a file that holds the number of active devices that was last published. If the result from this run isn't the same as the number that was published last time then the new result is published.

--- count_users.sh
+++ count_users.sh
@@ -3,33 +3,34 @@
 #----------------------------------------------------------------------------------------------------------------------------------------------------------
 #
 # Filename : counting_users.sh
-# Description: A bash script for outputting network's active mac addresses and the number of active devices to two files.
+# Description: Shell script for publishing network's daily number of unique mac addresses and the number of active devices to two MQTT paths.
 # Author: Alexandros Dorodoulis
-# Requirements: nmap
+# Requirements: nmap, mosquitto_pub
 #
 #----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 function usage(){
   echo "Possible arguments:
-          -d or --directory         define output directory
           -fn or --filename         define filename
           -a or --active            define the number of always active devices on the network (routers etc.)"
 }
 
 alwaysActiveDevices=2 # Number of always active devices.
-outputDirectory="/var/www/html" # Change this directory in order to change where the script outputs the files
 fileName="hackers.txt" # The name of the outputted file
 currentDirectory=$(pwd)
+
+#Read the numer of active devices that th
+if [[ -e $currentDirectory$fileName ]]; then
+  read previousActiveDevices < $currentDirectory/$fileName.txt
+else
+  previousActiveDevices=999999999
+fi
 
 # Parse parameters
 while [[ $# > 0 ]]
   do
   key="$1"
   case $key in
-      -d|--directory)
-        outputDirectory="$2"
-        shift
-      ;;
       -fn|--filenames)
         fileName="$2"
         shift
@@ -46,20 +47,18 @@ while [[ $# > 0 ]]
   shift
 done
 
-# Check if the directory exists, if it doesn't create it.
-if [[ ! -d $outputDirectory ]]; then
-  mkdir -p $outputDirectory
-fi
-
 networksIp=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}') # Find the ip and the subnetwork that nmap should scan.
 subnetwork=$(echo $networksIp| awk -F/ '{print $2}') # Get network's number of mask bits.
 networksIp=$(echo $networksIp | awk -F. '{OFS="."; $4='0'; print $0}')/$subnetwork # Replace the 4th ip octave with 0.
-sudo nmap -sP $networksIp --disable-arp-ping | grep ..:..:..:..:..:.. | awk '{print $3}' > $outputDirectory/mac_addresses.txt # Write mac addresses to mac_addresses.txt
-activeDevices=$(wc -l < $outputDirectory/mac_addresses.txt) # Count the active devices
+sudo nmap -sP $networksIp --disable-arp-ping | grep ..:..:..:..:..:.. | awk '{print $3}' > $currentDirectory/mac_addresses.txt # Write mac addresses to mac_addresses.txt
+activeDevices=$(wc -l < $currentDirectory/mac_addresses.txt) # Count the active devices
 actualDevices=$(($activeDevices-$alwaysActiveDevices)) # The actual number of active devices
 
-# If the device count returns 0 active devices check again just to be sure
+# If the device count returns 0 active devices check again
+# If not and the number of activeDevices is different
 if [[ $actualDevices -le 0 ]]; then
   $currentDirectory/count_users_with_ping.sh -d $outputDirectory -fn $fileName -a $alwaysActiveDevice -fc $actualDevices
+elif [[ $activeDevices -ne $previousActiveDevices ]]; then
+  mosquitto_pub -h www.techministry.gr -u EnterYourUsernameHere -P aSuperSecurePassword --cafile PathToYourCA -t "techministry/spacestatus/hackers" -m "$actualDevices" # Publish the number of active users through MQTT
+  echo $actualDevices > $currentDirectory/$fileName
 fi
-mosquitto_pub -h www.techministry.gr -u EnterYourUsernameHere -P aSuperSecurePassword --cafile PathToYourCA -t "techministry/spacestatus/hackers" -m "$actualDevices" # Publish the number of active users through MQTT
